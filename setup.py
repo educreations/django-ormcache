2dd6550a4fc06cd8032b04cb50d576fd64d534f9
Chris Streeter <chris@chrisstreeter.com>
2013-09-20 10:08:28 -0700

    Update the setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 readme_text = open("README.rst", "rb").read()
@@ -6,7 +8,7 @@ setup(
     name="django-ormcache",
     version="0.1",
     description="ORM cache for Django",
-    license="BSD",
+    license="MIT",
     keywords="cache django",
     author="Corey Farwell",
     author_email="coreyf@rwell.org",
@@ -17,9 +19,12 @@ setup(
     packages=["ormcache"],
     package_dir={"ormcache": "ormcache"},
     classifiers=[
+        'Framework :: Django',
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
+        'License :: OSI Approved :: MIT License',
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
     ]
 )
