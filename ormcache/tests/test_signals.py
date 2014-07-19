468ed7c9c49c41e3aa3092e4588f4e1e78fb3ee2
Corey Farwell <corey@educreations.com>
2014-07-19 00:12:05 -0700

    Remove Django mock dependency

--- ormcache/tests/test_signals.py
+++ ormcache/tests/test_signals.py
@@ -9,32 +9,41 @@ from ormcache.tests.testapp.models import CachedDummyModel
 class SignalsTestCase(TestCase):
 
     def setUp(self):
+        self.call_count = 0
         self.instance_pk = CachedDummyModel.objects.create().pk
         cache.clear()
 
-    def test_cache_hit_signal(self):
-        with mock_signal_receiver(cache_hit, sender=CachedDummyModel) as receiver:
-            # Cache miss
-            CachedDummyModel.objects.get(pk=self.instance_pk)
-            self.assertEquals(0, receiver.call_count)
+    def _signal_receiver(self):
+        def inner(*args, **kwargs):
+            self.call_count += 1
+        return inner
 
-            # Cache hit
-            CachedDummyModel.objects.get(pk=self.instance_pk)
-            self.assertEquals(1, receiver.call_count)
+    def test_cache_hit_signal(self):
+        cache_hit.connect(self._signal_receiver(), weak=False)
+
+        # Cache miss
+        CachedDummyModel.objects.get(pk=self.instance_pk)
+        self.assertEquals(0, self.call_count)
+
+        # Cache hit
+        CachedDummyModel.objects.get(pk=self.instance_pk)
+        self.assertEquals(1, self.call_count)
 
     def test_cache_missed_signal(self):
-        with mock_signal_receiver(cache_missed, sender=CachedDummyModel) as receiver:
-            # Cache miss
-            CachedDummyModel.objects.get(pk=self.instance_pk)
-            self.assertEquals(1, receiver.call_count)
+        cache_missed.connect(self._signal_receiver(), weak=False)
+
+        # Cache miss
+        CachedDummyModel.objects.get(pk=self.instance_pk)
+        self.assertEquals(1, self.call_count)
 
     def test_cache_invalidated_signal(self):
-        with mock_signal_receiver(cache_invalidated, sender=CachedDummyModel) as receiver:
-            # Cache miss
-            instance = CachedDummyModel.objects.get(pk=self.instance_pk)
-            self.assertEquals(0, receiver.call_count)
+        cache_invalidated.connect(self._signal_receiver(), weak=False)
 
-            # Save the object
-            instance.title = "hello"
-            instance.save()  # invalidate
-            self.assertEquals(1, receiver.call_count)
+        # Cache miss
+        instance = CachedDummyModel.objects.get(pk=self.instance_pk)
+        self.assertEquals(0, self.call_count)
+
+        # Save the object
+        instance.title = "hello"
+        instance.save()  # invalidate
+        self.assertEquals(1, self.call_count)
