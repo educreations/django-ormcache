21ae77dc16cbdce7ea2b4fa6d84f15a065119b89
Chris Streeter <chris@educreations.com>
2022-11-26 20:57:27 -0500

    Update for django 2

--- ormcache/utils.py
+++ ormcache/utils.py
@@ -2,7 +2,8 @@ try:
     from django.db.models.fields.related import SingleRelatedObjectDescriptor
 except ImportError:
     from django.db.models.fields.related_descriptors import (
-        ReverseOneToOneDescriptor as SingleRelatedObjectDescriptor)
+        ReverseOneToOneDescriptor as SingleRelatedObjectDescriptor,
+    )
 
 
 def _distinct(l):
@@ -19,29 +20,30 @@ def attach_foreignkey(objects, field):
 
     if not is_foreignkey:
         field = field.field
-        accessor = '_%s_cache' % field.name
-        model = field.rel.to
-        lookup = '%s__in' % 'pk'
+        accessor = "_%s_cache" % field.name
+        model = field.remote_field.model
+        lookup = "%s__in" % "pk"
         column = field.column
-        key = 'pk'
+        key = "pk"
     else:
         accessor = field.cache_name
         field = field.related.field
         model = field.model
-        lookup = '%s__in' % field.name
-        column = 'pk'
+        lookup = "%s__in" % field.name
+        column = "pk"
         key = field.column
 
     # Ensure values are unique, do not contain already present values, and
     # are not missing values specified in select_related
-    values = _distinct(getattr(o, column) for o in objects if
-                       getattr(o, accessor, False) is False)
+    values = _distinct(
+        getattr(o, column) for o in objects if getattr(o, accessor, False) is False
+    )
     if not values:
         return
 
     # Pull instances from cache if caching is enabled. This conditional logic
     # can be removed when the UserManager inherits BaseManager
-    if getattr(model, 'cache_enabled', False) is True:
+    if getattr(model, "cache_enabled", False) is True:
         instances = model.objects.from_ids(values, lookup=lookup)
     else:
         instances = model.objects.filter(**{lookup: values})
