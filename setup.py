2e035206f5ead21e12e611f2aab0ee391bf3ed05
Chris Streeter <chris@educreations.com>
2022-11-26 23:43:58 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ readme_text = open("README.rst", "r").read()
 
 setup(
     name="django-ormcache",
-    version="1.2",
+    version="1.2.1",
     description="ORM cache for Django",
     license="MIT",
     keywords="cache django",
@@ -27,7 +27,7 @@ setup(
     long_description=readme_text,
     packages=["ormcache"],
     package_dir={"ormcache": "ormcache"},
-    install_requires=["Django>=1.9,<2.0"],
+    install_requires=["Django>=1.9,<4.0"],
     classifiers=[
         "Framework :: Django",
         "Intended Audience :: Developers",
